bcc76c31633b344ff2a4d2f3e5b1935b32220ec0
ChengEx <frf2isgood@gmail.com>
2021-11-07 23:49:08 +0800

    11/7-1

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,5 +1,6 @@
 import React,{ useState,useEffect } from 'react'
 import { Button, Form } from 'semantic-ui-react';
+import { useHistory, useLocation} from 'react-router-dom';
 import axios from 'axios';
 import {MessageExamplePositive ,MessageExampleNegative} from '../messageBox';
 import '../pages/Register.css'
@@ -12,14 +13,17 @@ const initialState = {
 }
 
 const Register = () =>{
-    const [ state, setState] = useState(initialState);
-    const { username,password,confirmPassword,email} = initialState;
+    // const { username,password,confirmPassword,email} = initialState;
+    // const [ state, setState] = useState(initialState);
+    const [ username, setUsername] = useState('');
+    const [ password, setPassword] = useState('');
+    const [ confirmPassword, setConfirmPassword] = useState('');
+    const [ email, setEmail] = useState('');
     
-    const onChange = (event)=>{
-       let { name, value} = event.target;
-       setState({...state,[name]: value});
-    }
-
+    
+    
+    
+    
     const addUser = async(data) =>{
         const response = await axios.post('http://localhost:5000/users',data);
         if(response === 200){
@@ -29,13 +33,28 @@ const Register = () =>{
 
     const onSubmit = (event)=>{
         event.preventDefault();
-        if(password === confirmPassword){
-            addUser(state);
-        }else{
+        console.log(username,password,confirmPassword,email)
+        if(password != confirmPassword){
             console.log("password are not same");
         }
+        if(!username || !password || !confirmPassword || !email){
+            console.log("Please input the value into each input");
+        }
+        else{
+            var postData ={};
+            postData.username = username;
+                postData.password = password;
+            postData.email = email;
+            addUser(postData);
+        }
         
-    }
+        //history.push("/");
+        
+    }   
+    // const onChange = (event)=>{
+    //     let { name, value} = event.target;
+    //     setState({...state,[name]: value});
+    // }
 
     return (
         <div style={{marginTop:"100px"}}>
@@ -44,39 +63,45 @@ const Register = () =>{
                 padding:"15px",
                 maxWidth:"400px",
                 alignContent:"center"
-            }}>
-                <label htmlFor="Username">Username</label>
+            }}
+            onSubmit={onSubmit}
+            >
+                <label htmlFor="username">Username</label>
                 <input
                     type="text"
                     id="username"
-                    name="username"
+                    //name="username"
                     placeholder="Enter Username..."
-                    onChange={onChange}
-                    value={username}/>
-                <label htmlFor="Password">Password</label>
+                    onChange={e=> setUsername(e.target.value)}
+                    //value={username}
+                    />
+                <label htmlFor="password">Password</label>
                 <input
                     type="text"
                     id="password"
                     name="password"
                     placeholder="Enter Password..."
-                    onChange={onChange}
-                    value={password}/>
-                <label htmlFor="ConfirmPassword">ConfirmPassword</label>
+                    onChange={e=>setPassword(e.target.value)}
+                    //value={password}
+                    />
+                <label htmlFor="confirmPassword">ConfirmPassword</label>
                 <input
                     type="text"
                     id="confirmPassword"
                     name="confirmPassword"
                     placeholder="Enter ConfirmPassword..."
-                    onChange={onChange}
-                    value={confirmPassword}/>
-                <label htmlFor="Email">Email</label>
+                    onChange={e=>setConfirmPassword(e.target.value)}
+                    //value={confirmPassword}
+                    />
+                <label htmlFor="email">Email</label>
                 <input
                     type="text"
                     id="email"
                     name="email"
                     placeholder="Enter Email..."
-                    onChange={onChange}
-                    value={email}/>
+                    onChange={e=>setEmail(e.target.value)}
+                    //value={email}
+                    />
                 <input type="submit" value="Add"/>
 
             </form>
--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const postsRoute = require('./routes/posts');
 const usersRoute = require('./routes/users');
+const cors = require('cors');
 require('dotenv/config');
 
-
+server.use(cors());
 server.use(bodyParser.json());
 server.use('/posts',postsRoute);
 server.use('/users',usersRoute);
